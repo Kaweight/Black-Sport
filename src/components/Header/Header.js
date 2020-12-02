e9aa9231d54a082255d7868c1c7db81e88e2d805
Kamil Mazelanik <kamilmaz9206@gmail.com>
2020-12-02 09:46:57 +0100

    add center text in header, create new section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles'
-import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import { AppBar, Collapse, IconButton, Toolbar } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
   appbar: {
     background: 'none',
+    fontFamily: 'Nunito',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
   },
   appbarTitle: {
+    color: '#ffffff',
     flexGrow: '1',
   },
   icon: {
-    color: '#fff',
+    color: '#C4E538',
     fontSize: '2rem',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  containerTitle: {
+    color: '#ffffff',
+    fontFamily: 'Nunito',
+    fontSize: '3rem',
+  },
+  goDown: {
+    color: '#C4E538',
+    fontSize: '3rem',
   },
 }));
 
 export default function Header() {
   const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, [])
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar className={classes.appbar} elevation={0}>
-        <Toolbar>
+        <Toolbar className={classes.appbarWrapper}>
           <h1 className={classes.appbarTitle}>Speedway</h1>
           <IconButton>
             <SortIcon className={classes.icon} />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={45}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.containerTitle}>
+            lorem ipsum dolor
+      </h1>
+          <IconButton>
+            <ExpandMoreIcon className={classes.goDown} />
+          </IconButton>
+        </div>
+      </Collapse>
     </div>
   );
 }
---
+++ src/components/SectionSGP/SectionSGP.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+export default function SectionSGP() {
+  const classes = useStyles();
+  return (
+    <div>
+
+    </div>
+  )
+}
